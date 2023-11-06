3a8073997f2a02c5a01c973bf42551463f9cb679
aescastro <adriana.e.castro@outlook.com>
2023-11-05 23:30:29 -0800

    reading tracker with yup validation

--- src/Widgets/ReadingTracker.js
+++ src/Widgets/ReadingTracker.js
@@ -9,8 +9,23 @@ import {
     Form,
     Field
 } from "formik";
+import {
+    object,
+    string,
+    number,
+} from "yup";
 
 import { Widget } from "./Widget";
+
+const readingSchema = object({
+    book: string(),
+    currentPage: number().positive().integer().required("Required"),
+    totalPages: number().positive().integer().required("Required").when("currentPage", (currentPage, schema) => {
+        return schema.test("totalPages", "Total pages cannot be less than current pages", (totalPages) => {
+            return totalPages >= currentPage;
+        });        
+    }),
+});
 
 const ReadingTracker = () => {
     const [isView, setIsView] = useState(true);
@@ -34,85 +49,93 @@ const ReadingTracker = () => {
             <Formik
                 initialValues={values}
                 onSubmit={(values) => setValues(values)}
+                validationSchema={readingSchema}
             >
-                <Form
-                    style={{
-                        width: "100%",
-                    }}
-                >
-                    <Stack 
-                        gap={3}
-                        style={{
-                            justifyContent: "center",
-                            padding: "30px 19px",
-                            boxSizing: "border-box",
-                        }}
-                    >
-                        <div>
-                            <h5>Currently Reading</h5>
-                            <FormControl 
-                                plaintext={isView}
-                                readOnly={isView}
-                                name="book"
-                                as={Field}
-                            />    
-                        </div>
-                        
-                        <Stack 
-                            direction="horizontal"
+                {
+                    formik => (
+                        <Form
                             style={{
-                                alignSelf: "start",
                                 width: "100%",
-                                gap: "9px",
                             }}
                         >
-                            <ProgressBar 
-                                label={`${percent}%`}
-                                now={percent}
-                                style={{
-                                    flexGrow: 1,
-                                }}
-                            />
-                            <button onClick={() => setIsView(!isView)}>{isView ? "Edit" : "Save"}</button>
-                        </Stack>
-
-                        {
-                            !isView &&
-                            <Stack
-                                direction="horizontal"
-                                alignItems="center"
+                            <Stack 
+                                gap={3}
                                 style={{
                                     justifyContent: "center",
-                                    gap: "5px",
+                                    padding: "30px 30px",
+                                    boxSizing: "border-box",
                                 }}
                             >
-                                <span>On page</span>
-                                <FormControl
-                                    plaintext={isView}
-                                    readOnly={isView}
-                                    name="currentPage"
-                                    as={Field}
-                                    size="sm"
+                                <div>
+                                    <h5>Currently Reading</h5>
+                                    <FormControl 
+                                        plaintext={isView}
+                                        readOnly={isView}
+                                        name="book"
+                                        as={Field}
+                                    />    
+                                </div>
+                                
+                                <Stack 
+                                    direction="horizontal"
                                     style={{
-                                        width: "5em",
+                                        alignSelf: "start",
+                                        width: "100%",
+                                        gap: "9px",
                                     }}
-                                />
-                                <span>of</span>
-                                <FormControl
-                                    plaintext={isView}
-                                    readOnly={isView}
-                                    name="totalPages"
-                                    as={Field}
-                                    size="sm"
-                                    style={{
-                                        width: "5em",
-                                    }}
-                                />
-                            </Stack>
-                        } 
-                        
-                    </Stack>      
-                </Form>
+                                >
+                                    <ProgressBar 
+                                        label={`${percent}%`}
+                                        now={percent}
+                                        style={{
+                                            flexGrow: 1,
+                                        }}
+                                    />
+                                    <button type="submit" onClick={() => setIsView(!isView)}>{isView ? "Edit" : "Save"}</button>
+                                </Stack>
+
+                                {
+                                    !isView &&
+                                    <Stack
+                                        direction="horizontal"
+                                        style={{
+                                            justifyContent: "center",
+                                            gap: "5px",
+                                            alignItems: "center"
+                                        }}
+                                    >
+                                        <span>On page</span>
+                                        <FormControl
+                                            plaintext={isView}
+                                            readOnly={isView}
+                                            name="currentPage"
+                                            as={Field}
+                                            isInvalid={formik.errors.currentPage && formik.touched.currentPage}
+                                            size="sm"
+                                            style={{
+                                                width: "5em",
+                                            }}
+                                        />
+                                        <span>of</span>
+                                        <FormControl
+                                            plaintext={isView}
+                                            readOnly={isView}
+                                            name="totalPages"
+                                            isInvalid={formik.errors.totalPages && formik.touched.totalPages}
+                                            as={Field}
+                                            size="sm"
+                                            style={{
+                                                width: "5em",
+                                            }}
+                                        />
+                                    </Stack>
+                                } 
+                                
+                            </Stack>      
+                        </Form>  
+                    )
+                }
+                
                   
             </Formik>
         </Widget>
