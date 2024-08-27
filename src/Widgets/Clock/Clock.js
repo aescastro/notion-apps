891bc760015d7fe4f2cb0b2f7213fb64f6a689e7
aescastro <adriana.e.castro@outlook.com>
2024-08-26 19:09:13 -0700

    timezone clock options

--- src/Widgets/Clock/Clock.js
+++ src/Widgets/Clock/Clock.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import Stack from '@mui/material/Stack';
-import moment from "moment";
+import moment from 'moment-timezone';
 import styled from "@emotion/styled";
 import { Widget } from "../Widget"; 
+import { useQuery } from "../../utils";
 
 const H1 = styled.h1`
     margin: 0;
@@ -17,15 +18,24 @@ const H2 = styled.h2`
 `
 
 const Clock = () => {
-    const [date, setDate] = useState(moment().format("dddd, MMMM Do"));
-    const [time, setTime] = useState(moment().format("hh:mm A"));
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    var query = useQuery();
 
     useEffect(() => {
-        setInterval(() => {
-            setDate(moment().format("dddd, MMMM Do"));
-            setTime(moment().format("hh:mm A"));
+        const intervalId = setInterval(() => {
+            if (query.has("timezone")) {
+                setDate(moment.tz(query.get("timezone")).format("dddd, MMMM Do"));
+                setTime(moment.tz(query.get("timezone")).format("hh:mm A"));
+            } else {
+                setDate(moment().format("dddd, MMMM Do"));
+                setTime(moment().format("hh:mm A"));
+            }
         }, 1000);
-    }, []);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, [query]);
 
 
     return (
