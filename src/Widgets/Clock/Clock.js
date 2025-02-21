467ba7a3a75e06471f1fef5ad92f59a13bca9147
aescastro <adriana.e.castro@outlook.com>
2025-02-21 13:08:27 -0800

    clock, no overflow

--- src/Widgets/Clock/Clock.js
+++ src/Widgets/Clock/Clock.js
@@ -9,13 +9,13 @@ import { useQuery } from "../../utils";
 
 const H1 = styled.h1`
     margin: 0;
-    font-size: calc(0.3 * ${(props) => props.height}px);
+    font-size: min(calc(0.32 * ${(props) => props.containerHeight}px), calc(0.213 * ${(props) => props.containerWidth}px));
     font-weight: bold;
 `;
 
 const H2 = styled.h2`
     margin: 0;
-    font-size: calc(0.12 * ${(props) => props.height}px);
+    font-size: min(calc(0.10 * ${(props) => props.containerHeight}px), calc(0.067 * ${(props) => props.containerWidth}px));  
     font-weight: 500;
 `
 
@@ -48,14 +48,15 @@ const Clock = () => {
                     alignItems: "center",
                     justifyContent: "center",
                     aspectRatio: "1.75 / 1",
-                    width: "100%",
+                    height: "100%",
+                    overflowY: "hidden",
                 }}
                 ref={observe}
             >
-                <H1 height={height} width={width}>
+                <H1 containerHeight={height} containerWidth={width}>
                     {time}
                 </H1>
-                <H2 height={height} width={width}>
+                <H2 containerHeight={height} containerWidth={width}>
                     {date}
                 </H2>  
             </Stack>
